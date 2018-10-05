7f3ff5e4806dae09458b71f20c56010049e746b6
pliantmeerkat <jackthomasbranch@yahoo.com>
2018-10-05 15:12:57 +0100

    1st commit

---
+++ src/main/java/com/JBank/src/StockTrainerApplication.java
@@ -0,0 +1,12 @@
+package com.JBank.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrainerApplication.class, args);
+	}
+}
