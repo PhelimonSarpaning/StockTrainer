7e683da48fd8b1e7043117578121517d353b3ba7
pliantmeerkat <jackthomasbranch@yahoo.com>
2018-10-12 19:05:27 +0100

    adding repository, adding mongo to settings

--- src/main/java/repositories/UsersRepository.java
+++ src/main/java/repositories/UsersRepository.java
@@ -1,9 +1,10 @@
 package repositories;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import model.Users;
 
 public interface UsersRepository extends MongoRepository<Users, String> {
-
+	Users findBy_id(ObjectId _id);
 }
